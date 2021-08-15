3899a2179eb7bd4b3adda49578f749b604b5b770
Lê Văn Lai <vlai17507@gmail.com>
2021-08-15 15:34:04 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome React Hook</h1>
+    </div>
+  );
+}
+
+export default App;
